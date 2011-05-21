9c69d258cd9c76b4315cfd62e885f8335ac53ed5
Joshua Button <deathnoodle@gmail.com>
2011-05-21 14:15:02 -0700

    Finished lesson 3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,6 +14,11 @@ describe PagesController do
       response.should have_selector("title", 
       :content => "Ruby on Rails Sample App | Home")
     end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 
   describe "GET 'contact'" do
@@ -21,12 +26,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", 
+      :content => "Ruby on Rails Sample App | Contact")
+    end
   end
   
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+      :content => "Ruby on Rails Sample App | About")
     end
   end
 
